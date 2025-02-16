ec9ec897485eb5f21ef04715a1474a2c326f0def
Vinay187534 <L00187534@atu.ie>
2025-02-16 23:31:03 +0000

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -35,14 +35,9 @@ variable "endpoint-public-access" {}
 variable "ondemand_instance_types" {
   default = ["t3a.medium"]
 }
-
-variable "spot_instance_types" {}
 variable "desired_capacity_on_demand" {}
 variable "min_capacity_on_demand" {}
 variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
 variable "addons" {
   type = list(object({
     name    = string
