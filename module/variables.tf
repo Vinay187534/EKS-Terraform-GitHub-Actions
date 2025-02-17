ecaaec370bcdfab7120fdffc0eed9b086c47fd3c
Vinay187534 <L00187534@atu.ie>
2025-02-17 19:59:04 +0000

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -35,8 +35,6 @@ variable "endpoint-public-access" {}
 variable "ondemand_instance_types" {
   default = ["t3a.medium"]
 }
-
-variable "spot_instance_types" {}
 variable "desired_capacity_on_demand" {}
 variable "min_capacity_on_demand" {}
 variable "max_capacity_on_demand" {}
